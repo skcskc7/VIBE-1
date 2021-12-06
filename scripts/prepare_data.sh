5f8cff16db0fb344179c9be87b8c6cdce0385373
Daewon-Hwang <59599085+Daewon-Hwang@users.noreply.github.com>
2021-12-06 19:14:29 +0900

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -5,7 +5,11 @@ cd data
 gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
 unzip vibe_data.zip
 rm vibe_data.zip
+
+# dw
+
 cd ..
+
 mv data/vibe_data/sample_video.mp4 .
 mkdir -p $HOME/.torch/models/
 mv data/vibe_data/yolov3.weights $HOME/.torch/models/
