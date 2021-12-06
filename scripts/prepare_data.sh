9f6c2b41fb2dc2e666739c07d798a767445a0df3
Daewon-Hwang <59599085+Daewon-Hwang@users.noreply.github.com>
2021-12-06 19:18:26 +0900

    Update prepare_data.sh

--- scripts/prepare_data.sh
+++ scripts/prepare_data.sh
@@ -6,7 +6,10 @@ gdown "https://drive.google.com/uc?id=1untXhYOLQtpNEy4GTY_0fL_H-k6cTf_r"
 unzip vibe_data.zip
 rm vibe_data.zip
 
-# dw
+# dw - download F/M SMPL models
+
+gdown --id 1hC_hgJCPtFQ72Pv5cnjr0BaieNAbb7zn
+gdown --id 1xxL-L0cL85jUz8MxfSEyQ5ai39E63X4r
 
 cd ..
 
