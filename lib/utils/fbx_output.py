b1ef788be151f1a2c415e39b9ae05eb832937a4f
Daewon-Hwang <59599085+Daewon-Hwang@users.noreply.github.com>
2021-12-10 14:19:17 +0900

    Update fbx_output.py

--- lib/utils/fbx_output.py
+++ lib/utils/fbx_output.py
@@ -136,12 +136,14 @@ def process_pose(current_frame, pose, trans, pelvis_position):
         bone = bones[bone_name_from_index[index]]
 
         bone_rotation = Matrix(mat_rot).to_quaternion()
-        quat_x_90_cw = Quaternion((1.0, 0.0, 0.0), radians(-90))
-        quat_z_90_cw = Quaternion((0.0, 0.0, 1.0), radians(-90))
-
+        #quat_x_90_cw = Quaternion((1.0, 0.0, 0.0), radians(-90))
+        #quat_z_90_cw = Quaternion((0.0, 0.0, 1.0), radians(-90))
+        quat_y_90_cw = Quaternion((0.0, 1.0, 0.0), radians(-90))
+        quat_x_180_cw = Quaternion((1.0, 0.0, 0.0), radians(-180))
+        
         if index == 0:
             # Rotate pelvis so that avatar stands upright and looks along negative Y avis
-            bone.rotation_quaternion = (quat_x_90_cw @ quat_z_90_cw) @ bone_rotation
+            bone.rotation_quaternion = (quat_y_90_cw @ quat_x_180_cw) @ bone_rotation
         else:
             bone.rotation_quaternion = bone_rotation
 
